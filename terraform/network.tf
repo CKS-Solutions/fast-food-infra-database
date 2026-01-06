7389551b254f5307dd4107d486d3191ba9a1fbf4
Kenzo Sato <kenzohideaky@gmail.com>
2026-01-06 00:55:44 -0300

    fix: update security group description to English

--- terraform/network.tf
+++ terraform/network.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "lambda" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Permitir todo tráfego de saída"
+    description = "Allow all outbound traffic"
   }
 
   tags = {
